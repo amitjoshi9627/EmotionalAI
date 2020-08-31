f6a84e30416ca87add854a63f1d75737b2a437e9
amitjoshi9627 <amitjoshi9627@gmail.com>
2020-08-31 21:27:45 +0530

    Minor Updates

--- src/streamlitapp.py
+++ src/streamlitapp.py
@@ -2,10 +2,10 @@ from sentiment_classifier import get_prediction
 import streamlit as st
 
 text = st.text_input("Enter Your Text..")
-result = get_prediction(text)
-if result:
-    prediction = "Positive Sentiment"
-else:
-    prediction = "Negative Sentiment"
-if text != "":
+if text is not None:
+    result = get_prediction(text)
+    if result:
+        prediction = "Positive Sentiment"
+    else:
+        prediction = "Negative Sentiment"
     st.write(prediction)
